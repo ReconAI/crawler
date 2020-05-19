5be7d23f4342035d18e71d0671d5882ea893e247
dmitry <dmitry.tsatsarin@gmail.com>
2020-05-20 01:02:41 +0300

    Fix test

--- tests/integration/tests_video_search.py
+++ tests/integration/tests_video_search.py
@@ -30,7 +30,7 @@ class VideoProjectApiTestCase(CommonTestCase):
         VideoProjectFactory.create(name='test1')
         vp = VideoProjectFactory.create(name='test2')
 
-        response = self.client.delete(f'/api/video-project/{vp.id}')
+        response = self.client.delete(f'/api/video-project/{vp.id}/')
         self.assertEqual(response.status_code, rest_status.HTTP_204_NO_CONTENT)
         self.assertEqual(VideoProject.objects.count(), 1)
 
