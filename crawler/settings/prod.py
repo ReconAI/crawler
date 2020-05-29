f500583ac2ab32bcb1def178e451325ce74791f5
dmitry <dmitry.tsatsarin@gmail.com>
2020-05-30 02:02:51 +0300

    Add some prod settings

--- crawler/settings/prod.py
+++ crawler/settings/prod.py
@@ -11,3 +11,9 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+SQS_CRAWLER_DOWNLOAD_VIDEO_FILE_QUEUE_NAME = 'prod-crawler-download-video-file-queue'
+CELERY_TASK_DEFAULT_QUEUE = 'prod-celery-default'
+CELERY_TASK_ROUTES = {
+    'apps.video_search.tasks.*': {'queue': SQS_CRAWLER_DOWNLOAD_VIDEO_FILE_QUEUE_NAME}
+}
