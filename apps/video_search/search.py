388e6780fb83524cbe581219b37d26e71b555d5d
dmitry <dmitry.tsatsarin@gmail.com>
2020-05-31 00:16:39 +0300

    Change defaults for Vimeo and Youtube search

--- apps/video_search/search.py
+++ apps/video_search/search.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 class TsdVimeoClient:
     client = None
     default_params = {
-        'per_page': 20
+        'per_page': 5
     }
 
     def __init__(self):
@@ -51,6 +51,6 @@ class TsdYoutubeClient:
         search_response = self.client.search().list(
             q=search_text,
             part='id,snippet',
-            maxResults=20
+            maxResults=5
         ).execute()
         return search_response
