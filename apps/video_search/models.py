2d3130f2476558285ba85cba0775391f9e4768b8
dmitry <dmitry.tsatsarin@gmail.com>
2020-05-31 20:18:14 +0300

    Rename tables

--- apps/video_search/models.py
+++ apps/video_search/models.py
@@ -10,9 +10,15 @@ class VideoStatusEnum(ChoiceEnum):
 class VideoProject(models.Model):
     name = models.CharField(max_length=255, help_text='name of project')
 
+    class Meta:
+        db_table = "apps_video_search__video_project"
+
 class VideoSearchResult(models.Model):
     project = models.ForeignKey(VideoProject, on_delete=models.CASCADE)
     source_link = models.URLField(help_text='Source vimeo/youtube link')
     link = models.FileField(upload_to='', null=True, help_text='saved link to our storage')
     preview_link = models.FileField(upload_to='', null=True, help_text='saved link for preveiw to our storage')
     status = models.CharField(choices=VideoStatusEnum.for_choice(), null=True, max_length=100)
+
+    class Meta:
+        db_table = "apps_video_search__search_result"
