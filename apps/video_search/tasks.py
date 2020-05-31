1eb67e552e69c152824c43b564d1ec7f0e49b71e
dmitry <dmitry.tsatsarin@gmail.com>
2020-06-01 02:20:18 +0300

    Small fix for async task

--- apps/video_search/tasks.py
+++ apps/video_search/tasks.py
@@ -17,24 +17,26 @@ logger = get_task_logger(__name__)
 @shared_task
 def task_save_source_video_and_create_preview(video_search_result_id):
     logger.info('video_search_result_id=%s' % video_search_result_id)
-    if not VideoSearchResult.objects.filter(id=video_search_result_id).exists():
-        logger.info("video_search_result_id=%s id deleted. Could not start video processing" % video_search_result_id)
-    video_search_result = VideoSearchResult.objects.filter(id=video_search_result_id).get()
-    out_filepath, out_filename = VideoDownloader().download(video_search_result.source_link)
-    preview_filepath, preview_filename = PreviewDispatcher().make_video(out_filepath)
 
-    # save to S3
-    f = open(out_filepath, 'rb')
-    dumped_file = File(f, name=out_filename)
-    preview_file = File(open(preview_filepath, 'rb'), name=preview_filename)
-    video_search_result.link = dumped_file
-    video_search_result.preview_link = preview_file
     try:
-        video_search_result.save()
-    except Exception as e:
-        logger.info("video_search_result_id=%s id deleted. Could not save video links" % video_search_result_id)
+        video_search_result = VideoSearchResult.objects.filter(id=video_search_result_id).get()
+        out_filepath, out_filename = VideoDownloader().download(video_search_result.source_link)
+        preview_filepath, preview_filename = PreviewDispatcher().make_video(out_filepath)
 
-    # delete files from folder
-    os.remove(out_filepath)
-    os.remove(preview_filepath)
+        # save to S3
+        f = open(out_filepath, 'rb')
+        dumped_file = File(f, name=out_filename)
+        preview_file = File(open(preview_filepath, 'rb'), name=preview_filename)
+        video_search_result.link = dumped_file
+        video_search_result.preview_link = preview_file
+        try:
+            video_search_result.save()
+        except Exception as e:
+            logger.info("video_search_result_id=%s id deleted. Could not save video links" % video_search_result_id)
+
+        # delete files from folder
+        os.remove(out_filepath)
+        os.remove(preview_filepath)
+    except VideoSearchResult.DoesNotExist as e:
+        logger.info("video_search_result_id=%s id deleted. Could not start video processing" % video_search_result_id)
 
